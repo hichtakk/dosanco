c87287dfdb3d309e5faa21723d32353fd0557f7d
Hirotaka Ichikawa <hichikaw@actapio.com>
2019-07-12 11:53:28 -0700

    override configuration with environmental variable

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"os"
 	"github.com/BurntSushi/toml"
 )
 
@@ -16,6 +17,11 @@ func NewConfig(path string) (Config, error) {
 	var conf Config
 	if _, err := toml.DecodeFile(path, &conf); err != nil {
 		return conf, err
+	}
+
+	// override configuration with environmental variables
+	if env_dbpath := os.Getenv("DOSANCO_DB"); env_dbpath != "" {
+		conf.DB.Path = os.Getenv("DOSANCO_DB")
 	}
 
 	return conf, nil
