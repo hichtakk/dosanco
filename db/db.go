12afe0a46ce0ce2ab9a16dd06ec8c3a99d1c6b74
Hirotaka Ichikawa <hichikaw@actapio.com>
2019-07-18 10:27:53 -0700

    add reserved flag

--- db/db.go
+++ db/db.go
@@ -48,55 +48,71 @@ func initNetwork() {
 		rootNetwork.ID = 1
 		rootNetwork.CIDR = "0.0.0.0/0"
 		rootNetwork.Description = "Root"
+		rootNetwork.Reserved = true
 		db.Create(&rootNetwork)
 	}
 	// reserve RFC6890
+	var hostNetwork model.IPv4Network
+	if result := db.Take(&hostNetwork, "id=2"); result.Error != nil {
+		hostNetwork.ID = 2
+		hostNetwork.CIDR = "0.0.0.0/8"
+		hostNetwork.SupernetworkID = 1
+		hostNetwork.Description = "RFC 1122,6890: This host on this network"
+		hostNetwork.Reserved = true
+		db.Create(&hostNetwork)
+	}
 	var loopbackNetwork model.IPv4Network
-	if result := db.Take(&loopbackNetwork, "id=2"); result.Error != nil {
-		loopbackNetwork.ID = 2
+	if result := db.Take(&loopbackNetwork, "id=3"); result.Error != nil {
+		loopbackNetwork.ID = 3
 		loopbackNetwork.CIDR = "127.0.0.0/8"
 		loopbackNetwork.SupernetworkID = 1
 		loopbackNetwork.Description = "RFC 6890: Loopback"
+		loopbackNetwork.Reserved = true
 		db.Create(&loopbackNetwork)
 	}
 	var linkLocalNetwork model.IPv4Network
-	if result := db.Take(&linkLocalNetwork, "id=3"); result.Error != nil {
-		linkLocalNetwork.ID = 3
+	if result := db.Take(&linkLocalNetwork, "id=4"); result.Error != nil {
+		linkLocalNetwork.ID = 4
 		linkLocalNetwork.CIDR = "169.254.0.0/16"
 		linkLocalNetwork.SupernetworkID = 1
 		linkLocalNetwork.Description = "RFC 3927: Link-local"
+		linkLocalNetwork.Reserved = true
 		db.Create(&linkLocalNetwork)
 	}
 	var protoAssignNetwork model.IPv4Network
-	if result := db.Take(&protoAssignNetwork, "id=4"); result.Error != nil {
-		protoAssignNetwork.ID = 4
+	if result := db.Take(&protoAssignNetwork, "id=5"); result.Error != nil {
+		protoAssignNetwork.ID = 5
 		protoAssignNetwork.CIDR = "192.0.0.0/24"
 		protoAssignNetwork.SupernetworkID = 1
 		protoAssignNetwork.Description = "RFC 6890: IETF protocol assignment"
+		protoAssignNetwork.Reserved = true
 		db.Create(&protoAssignNetwork)
 	}
 	var testNetwork model.IPv4Network
-	if result := db.Take(&testNetwork, "id=5"); result.Error != nil {
-		testNetwork.ID = 5
+	if result := db.Take(&testNetwork, "id=6"); result.Error != nil {
+		testNetwork.ID = 6
 		testNetwork.CIDR = "192.0.2.0/24"
 		testNetwork.SupernetworkID = 1
 		testNetwork.Description = "RFC 5737,6890: TEST-NET-1"
+		testNetwork.Reserved = true
 		db.Create(&testNetwork)
 	}
 	var testNetwork2 model.IPv4Network
-	if result := db.Take(&testNetwork2, "id=6"); result.Error != nil {
-		testNetwork2.ID = 6
+	if result := db.Take(&testNetwork2, "id=7"); result.Error != nil {
+		testNetwork2.ID = 7
 		testNetwork2.CIDR = "198.51.100.0/24"
 		testNetwork2.SupernetworkID = 1
 		testNetwork2.Description = "RFC 5737,6890: TEST-NET-2"
+		testNetwork2.Reserved = true
 		db.Create(&testNetwork2)
 	}
 	var testNetwork3 model.IPv4Network
-	if result := db.Take(&testNetwork3, "id=7"); result.Error != nil {
-		testNetwork3.ID = 7
+	if result := db.Take(&testNetwork3, "id=8"); result.Error != nil {
+		testNetwork3.ID = 8
 		testNetwork3.CIDR = "203.0.113.0/24"
 		testNetwork3.SupernetworkID = 1
 		testNetwork3.Description = "RFC 5737,6890: TEST-NET-3"
+		testNetwork3.Reserved = true
 		db.Create(&testNetwork3)
 	}
 }
