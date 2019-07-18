8667bd2f7e4c815d7bdf98ebc6f27fca39a581a8
Hirotaka Ichikawa <hichikaw@actapio.com>
2019-07-18 10:41:28 -0700

    split cmd files

---
+++ cmd/config.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+// DBConfig is struct for database configuration
+type APIServerConfig struct {
+	Url string `toml:"url"`
+}
+
+// Config is struct for main configuration
+type Config struct {
+	APIServer APIServerConfig `toml:"apiserver"`
+}
+
+func initConfig() {
+	if flagConfig != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(flagConfig)
+	} else {
+		// Search config in home directory
+		homedir := os.Getenv("HOME")
+		viper.AddConfigPath(homedir)
+		viper.SetConfigName(".dosanco.toml")
+	}
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Can't read config:", err)
+		os.Exit(1)
+	}
+	if err := viper.Unmarshal(&Conf); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
